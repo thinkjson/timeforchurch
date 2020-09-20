5529cf60b0015eec522f23885655e2eaef555116
Mark Cahill <markcahill@iheartmedia.com>
2020-09-20 11:22:48 -0400

    Minor tweaks

--- cron.py
+++ cron.py
@@ -23,23 +23,22 @@ safe = gatherings and crowds
 
 data = """
     <h1>Is it safe to return to church?</h1>
-    <div class="outcome {safe}">{is_safe}</div>
-    <ul>
-        <li>{gatherings}</li>
-        <li>{crowds}</li>
-        <li>{positivity}</li>
-    </ul>
+    <div class="outcome {safe_class}">{is_safe}</div>
+    {gatherings}
+    {crowds}
+    {positivity}
 
     <h1>Do I need to wear a mask?</h1>
-    <div class="outcome">{mask}</div>
+    <div class="outcome {mask_class}">{mask}</div>
     {mask_info}
 """.format(
-    safe="text-warning" if safe else "text-danger",
+    safe_class="text-warning" if safe else "text-danger",
     is_safe="NO" if not safe else "MAYBE",
-    gatherings="The Florida Department of Health recommends refraining from gatherings of more than 10 people" if not gatherings else "",
-    positivity="Positivity rate of {0} is too high".format(positivity) if not positivity_nominal else "",
-    crowds="The Florida Department of Health recommends avoiding crowds" if not crowds else "",
+    gatherings="""<div class="alert alert-warning" role="alert">The Florida Department of Health recommends refraining from gatherings of more than 10 people</div>""" if not gatherings else "",
+    positivity="""<div class="alert alert-warning" role="alert">Positivity rate of {0}%% is too high</div>""".format(positivity) if not positivity_nominal else "",
+    crowds="""<div class="alert alert-warning" role="alert">The Florida Department of Health recommends avoiding crowds</div>""" if not crowds else "",
     mask="YES" if masks else "UNKNOWN",
+    safe_class="text-danger" if masks else "text-warning",
     mask_info="<p>Check the <a href='https://floridahealthcovid19.gov/'>Florida Department of Health's COVID-19 response</a> for more information" if not masks else "",
 )
 
