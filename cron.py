f6981ac6d50975e945ca349738a0ade9717b88f4
Mark Cahill <markcahill@iheartmedia.com>
2021-01-24 10:14:26 -0500

    Update DOH guidance

--- cron.py
+++ cron.py
@@ -10,6 +10,7 @@ output_file = os.path.join(script_dir, "_includes/data.html")
 # Grab latest FDOH guidance
 doh = requests.get("https://floridahealthcovid19.gov/prevention/").text
 masks_doh = "Cover your mouth and nose" in doh
+crowds_doh = "large gatherings" in doh
 
 # Grab R0 data
 r0 = requests.get("https://d14wlfuexuxgcm.cloudfront.net/covid/rt.csv").text
@@ -29,14 +30,15 @@ scorecard = requests.get(
     "https://thefloridascorecard.org/pillar&c=64&pillar=0").text
 positivity = 0.0
 try:
-    positivity = float(re.search(
-        'data-target="#covid-percent-positiveModal">([0-9.]+)%</a>', scorecard).group(1))
+    positivity = float(
+        re.search('data-target="#covid-percent-positiveModal">([0-9.]+)%</a>',
+                  scorecard).group(1))
 except:
     pass
 positivity_nominal = positivity > 0.0 and positivity < 3.0
 
 # Determine safe to return
-safe = masks_r0 < 1.0
+safe = masks_r0 < 1.0 and not crowds_doh
 
 data = """
     <h1>Is it safe to return to church?</h1>
@@ -44,7 +46,7 @@ data = """
     {positivity}
     {r0}
 
-    <p>NOTE: Guidance from the Florida Department of Health no longer includes language about avoiding crowds.</p>
+    {crowds_doh}
 
     <h1>Do I need to wear a mask?</h1>
     <div class="outcome {mask_class}">{mask}</div>
@@ -54,13 +56,21 @@ data = """
 """.format(
     safe_class="text-warning" if safe else "text-danger",
     is_safe="NO" if not safe else "MAYBE",
-    positivity="""<div class="alert alert-danger" role="alert">Positivity rate of {0}% is above the 5.0% threshold for reopening announced in April 2020</div>""".format(
-        positivity) if not positivity_nominal else "",
+    crowds_doh=
+    """<p>NOTE: Guidance from the Florida Department of Health says "If you are around other people, keep 6 feet between you when possible. Avoid hugs, handshakes, large gatherings and close quarters."</p>"""
+    if crowds_doh else "",
+    positivity=
+    """<div class="alert alert-danger" role="alert">Positivity rate of {0}% is above the 5.0% threshold for reopening announced in April 2020</div>"""
+    .format(positivity) if not positivity_nominal else "",
     mask="YES" if masks else "UNKNOWN",
     mask_class="text-success" if masks else "text-warning",
-    r0=("""<div class="alert alert-danger" role="alert">R0 of %.2f is greater than 1.0 in the state of Florida</div>""" %
-        masks_r0) if masks_r0 > 1.0 else "",
-    mask_info="<p>Check the <a href='https://floridahealthcovid19.gov/prevention/'>Florida Department of Health's COVID-19 response</a> for more information" if not masks_doh else """<div class="alert alert-info" role="alert">The Florida Department of Health recommends wearing a face covering when in public</div>""",
+    r0=
+    ("""<div class="alert alert-danger" role="alert">R0 of %.2f is greater than 1.0 in the state of Florida</div>"""
+     % masks_r0) if masks_r0 > 1.0 else "",
+    mask_info=
+    "<p>Check the <a href='https://floridahealthcovid19.gov/prevention/'>Florida Department of Health's COVID-19 response</a> for more information"
+    if not masks_doh else
+    """<div class="alert alert-info" role="alert">The Florida Department of Health recommends wearing a face covering when in public</div>""",
     updated=datetime.now().strftime("%A, %B %d at %H:%M %p"),
 )
 
